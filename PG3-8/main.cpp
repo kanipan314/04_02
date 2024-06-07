59d196dd17be1d7f88a51ec86b38ea2c2df7ec7f
kanipan314 <k023g1020@g.neec.ac.jp>
2024-06-07 15:24:01 +0900

    パターンを増やした

--- PG3-8/main.cpp
+++ PG3-8/main.cpp
@@ -8,10 +8,17 @@ int main() {
 	MinTemplate<int> Intptr(10, 20);
 	MinTemplate<double> Doubleptr(1.2, 3.4);
 	MinTemplate<float> Floatptr(3.14f, 2.81f);
+	MinTemplate<int> IntDoubleptr(10, 9.2);
+	MinTemplate<double> DoubleFloatptr(1.2, 3.4f);
+	MinTemplate<float> FloatIntptr(3.14f, 281);
+
 
 	printf("最小値 : %d\n", Intptr.Min());
 	printf("最小値 : %lf\n", Doubleptr.Min());
 	printf("最小値 : %f\n", Floatptr.Min());
+	printf("最小値 : %d\n", IntDoubleptr.Min());
+	printf("最小値 : %lf\n", DoubleFloatptr.Min());
+	printf("最小値 : %f\n", FloatIntptr.Min());
 
 	return 0;
 }
